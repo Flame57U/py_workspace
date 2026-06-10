7c3ad5822f58de6e5afa8bfafe236a59d81ea35c
Yongjj <yongjunjie@yeah.net>
2026-06-10 19:53:50 +0800

    django

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-f = 1
+f = "Begin"
 print(f" {f}")
+user_prompt = input("ds: ")
+a = float(input("数字: "))
+print(f"{round(a/7,1)} cm")
+
+print(f"用户输入: {user_prompt}\n" + "="*40)
+
+if __name__ == '__main__':
+    a = input("ENTER your weight: ")
+    print(f"{a} Lbs.")
