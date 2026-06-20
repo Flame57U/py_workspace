addf89729d8ca506eeac53e0e995d8ed6e5cf7c7
Yongjj <yongjunjie@yeah.net>
2026-06-21 00:35:34 +0800

    测试api转大小写

--- DJ/mytest/serializers.py
+++ DJ/mytest/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from mytest.models import Music
 
+class ToUpperCaseCharField(serializers.CharField):
+    def to_representation(self, value):
+        return value.upper()
 
 class MusicSerializer(serializers.ModelSerializer):
+    singer = ToUpperCaseCharField()
+
     class Meta:
         model = Music
         # fields = '__all__'  全部 fields
