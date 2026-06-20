29cc301eb48992e84e9e20f436aa9f89e678f533
Yongjj <yongjunjie@yeah.net>
2026-06-21 00:12:29 +0800

    注册urls

--- DJ/mytest/urls.py
+++ DJ/mytest/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
+from .models import Music
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register(r'music', Music)
 urlpatterns = [
     path('', views.mytest, name = 'mytest'),
     path('login/', views.login, name = 'login'),
     path('hallo/', views.hello, name='hello_view'),
+
+    path('api/', include(router.urls)),
 ]
