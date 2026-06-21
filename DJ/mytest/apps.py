e50d99dc5760b04095e4c522ecf9bacf6d0a00c0
Yongjj <yongjunjie@yeah.net>
2026-06-21 21:53:27 +0800

    classic 99 table

--- DJ/mytest/apps.py
+++ DJ/mytest/apps.py
@@ -5,3 +5,10 @@ if __name__ == '__main__':
     print(a('true'))   # 字符串
     print(a(True))     # 布尔值
     print(a(0))        # 整数
+
+    for y in range(1, 10):
+        for x in range(1, 10):
+            if x > y:
+                break
+            print(f'{y}*{x}={y*x}', end=' ')
+        print()
